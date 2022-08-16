c31bc7a4eb2c684302352f54e6eb4c7ef6023489
Lucas Coura <106277330+lukecoura@users.noreply.github.com>
2022-08-16 18:36:56 -0300

    ex alura

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
