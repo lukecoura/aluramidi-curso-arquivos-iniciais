03187c14ed69f6b1c17ac2c5835b74db572eb71e
Lucas Coura <106277330+lukecoura@users.noreply.github.com>
2022-08-18 23:05:01 -0300

    js alura - estrutura for

--- main.js
+++ main.js
@@ -4,10 +4,7 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador <= listaDeTeclas.length) {
-
+for (let contador = 0; contador <= listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
 
     const instrumento = tecla.classList[1];
@@ -17,6 +14,4 @@ while (contador <= listaDeTeclas.length) {
     tecla.onclick = function() {
         tocaSom(idAudio);
     }
-
-    contador += 1;
 }
