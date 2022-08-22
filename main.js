eeeca5360a96275e1875b7fe7fea011c89d61ecc
Lucas Coura <106277330+lukecoura@users.noreply.github.com>
2022-08-22 14:37:02 -0300

    estrutura condicional aplicada ao evento onkeydown

--- main.js
+++ main.js
@@ -5,18 +5,25 @@ function tocaSom(idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let contador = 0; contador <= listaDeTeclas.length; contador++) {
+    
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function() {
         tocaSom(idAudio);
     }
-    tecla.onkeydown = function() {
-        tecla.classList.add('ativa');
+
+    tecla.onkeydown = function(evento) {
+
+        console.log(evento.code)
+
+        if (evento.code === 'Space' || evento.code === 'Enter' || evento.code === 'NumpadEnter') {
+            tecla.classList.add('ativa');
+        }
+        
     }
+
     tecla.onkeyup = function() {
         tecla.classList.remove('ativa');
     }
